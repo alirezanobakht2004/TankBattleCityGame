52d84bded2c953eff42be357878a04b39fc2fdf1
alirezanobakht2004 <alireza.nobakht2004@gmail.com>
2023-06-27 13:32:03 +0330

    start menu created

--- src/main/java/ir/ac/kntu/GUI/TankBattleCity.java
+++ src/main/java/ir/ac/kntu/GUI/TankBattleCity.java
@@ -1,15 +1,50 @@
 package ir.ac.kntu.GUI;
 
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.util.Scanner;
 
 public class TankBattleCity {
 
-    private GridPane presentPane;
+    private Stage stage;
 
-    public void addNodesToPane(Pane pane){
-        pane.getChildren().add(presentPane);
+    private GridPane startPane = new GridPane();
+
+    private GridPane menuPane = new GridPane();
+
+    public TankBattleCity(Stage stage) {
+        this.stage = stage;
     }
+
+    public void start() {
+        startPane.setPrefSize(600, 600);
+        ColumnConstraints col1 = new ColumnConstraints();
+        col1.setPercentWidth(20);
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setPercentWidth(60);
+        ColumnConstraints col3 = new ColumnConstraints();
+        col3.setPercentWidth(20);
+        RowConstraints row1 = new RowConstraints();
+        row1.setPercentHeight(50);
+        RowConstraints row2 = new RowConstraints();
+        row2.setPercentHeight(50);
+        startPane.getColumnConstraints().addAll(col1, col2, col3);
+        startPane.getRowConstraints().addAll(row1, row2);
+        Image image = new Image("images/beforeStart.png");
+        ImageView imageView = new ImageView(image);
+        startPane.add(imageView, 1, 0);
+        startPane.setStyle("-fx-background-color: black;");
+        Scene scene = new Scene(startPane);
+        stage.setScene(scene);
+    }
+
 
 }
--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -3,8 +3,11 @@ package ir.ac.kntu;
 import ir.ac.kntu.GUI.TankBattleCity;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.util.Scanner;
 
 public class Main extends Application {
 
@@ -14,12 +17,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        TankBattleCity tankBattleCity = new TankBattleCity();
-        Pane fatherPane = new Pane();
-        fatherPane.setPrefSize(650,650);
-        tankBattleCity.addNodesToPane(fatherPane);
-        Scene scene = new Scene(fatherPane);
-        primaryStage.setScene(scene);
+        TankBattleCity tankBattleCity = new TankBattleCity(primaryStage);
+        tankBattleCity.start();
         primaryStage.show();
     }
 }
